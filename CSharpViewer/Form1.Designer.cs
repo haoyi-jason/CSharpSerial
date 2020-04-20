99d0843320f4c00155a1c8f87f0b252dd6ae1a7f
Jason <jjyeh1976@gmail.com>
2020-04-20 19:26:46 +0800

    加入專案檔案。

---
+++ CSharpViewer/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace CSharpViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnWriteParam = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbPort = new System.Windows.Forms.ComboBox();
+            this.cbBaud = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(222, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(498, 314);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnWriteParam
+            // 
+            this.btnWriteParam.Location = new System.Drawing.Point(106, 423);
+            this.btnWriteParam.Name = "btnWriteParam";
+            this.btnWriteParam.Size = new System.Drawing.Size(147, 23);
+            this.btnWriteParam.TabIndex = 1;
+            this.btnWriteParam.Text = "Write Parameters";
+            this.btnWriteParam.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.cbBaud);
+            this.groupBox1.Controls.Add(this.cbPort);
+            this.groupBox1.Location = new System.Drawing.Point(16, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 121);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Port Config";
+            // 
+            // cbPort
+            // 
+            this.cbPort.FormattingEnabled = true;
+            this.cbPort.Location = new System.Drawing.Point(73, 21);
+            this.cbPort.Name = "cbPort";
+            this.cbPort.Size = new System.Drawing.Size(121, 20);
+            this.cbPort.TabIndex = 0;
+            // 
+            // cbBaud
+            // 
+            this.cbBaud.FormattingEnabled = true;
+            this.cbBaud.Items.AddRange(new object[] {
+            "38400"});
+            this.cbBaud.Location = new System.Drawing.Point(73, 47);
+            this.cbBaud.Name = "cbBaud";
+            this.cbBaud.Size = new System.Drawing.Size(121, 20);
+            this.cbBaud.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(114, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Baudrate";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 506);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnWriteParam);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnWriteParam;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cbBaud;
+        private System.Windows.Forms.ComboBox cbPort;
+    }
+}
+
---
+++ CSharpViewer/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace CSharpViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            DataTable tb1 = new DataTable("table1");
+            tb1.Columns.Add(new DataColumn("CH Name"));
+            tb1.Columns.Add("Value",System.Type.GetType("System.Double"));
+            tb1.Columns.Add(new DataColumn("Unit"));
+            //tb1.Columns.Add("COL",)
+            tb1.Columns.Add("Enable",System.Type.GetType("System.Boolean"));
+
+            tb1.Rows.Add(new object[] { "Acceleration X", 0.0, "m/s^2", false });
+            tb1.Rows.Add(new object[] { "Acceleration Y", 0.0, "m/s^2", false });
+            tb1.Rows.Add(new object[] { "Acceleration Z", 0.0, "m/s^2", false });
+            tb1.Rows.Add(new object[] { "roll 角加速度(MTI)", 0.0, "deg/s^2", false });
+            tb1.Rows.Add(new object[] { "yaw 角加速度(MTI)", 0.0, "deg/s^2", false });
+            tb1.Rows.Add(new object[] { "pitch 角加速度(MTI)", 0.0, "deg/s^2", false });
+
+            //DataRow r = tb1.NewRow();
+            //r[0] = "CH";
+            //r[1] = "value";
+            //r[2] = "unit";
+            //r[3] = false;
+            //tb1.Rows.Add(r);
+
+            //object[] rows =
+            //{
+            //    new object[]{"CH2","VAL2","UNIT3",false},
+            //    new object[]{"CH4","VAL4","UNIT4",false},
+            //};
+
+            dataGridView1.DataSource = tb1;
+
+            cbPort.Items.AddRange(SerialPort.GetPortNames());
+
+        }
+
+    }
+}
